93f23c972b5a76f5d100dc41578f0d3d8ff7ef66
yuli.kamakura <barnett.yuxiang@gmail.com>
2024-08-06 20:39:32 +0800

    feat: Add player_health variable and drink_potion function

--- course/udemy/100-days-of-code/day12/task.py
+++ course/udemy/100-days-of-code/day12/task.py
@@ -7,3 +7,13 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+# Global Scope
+player_health = 10
+
+def drink_potion():
+    potion_strength = 2
+    print(player_health)
+
+drink_potion()
+print(player_health)
